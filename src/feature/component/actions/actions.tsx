b9cfae8a1a6e241b1073308dc85a7017f0c5e32b
Fabio <Fabiodn.798@gmail.com>
2023-02-23 00:34:37 +0100

    Fixing netlify problem

--- src/feature/component/actions/actions.tsx
+++ src/feature/component/actions/actions.tsx
@@ -1,44 +1,45 @@
-import React, { useState, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function Actions(props: any) {
-  const [callState, setCallState] = useState("idle");
-
-  useEffect(() => {
-    if (callState === "calling") {
-      const timerId = setTimeout(() => {
-        endCall();
-      }, 5000);
-
-      return () => {
-        clearTimeout(timerId);
-      };
-    }
-  }, [callState]);
-
-  const startCall = () => {
-    if (props.phoneNumber.length === 9) {
-      setCallState("calling");
-      props.setPhoneNumber("");
-    }
-  };
-
-  const endCall = () => {
-    setCallState("idle");
-    props.setPhoneNumber("");
-  };
-
-  return (
-    <div className="actions">
-      {callState === "idle" && (
-        <button className="call-button" onClick={startCall}>
-          Call
-        </button>
-      )}
-      {callState === "calling" && (
-        <button className="hang-button" onClick={endCall}>
-          Hang
-        </button>
-      )}
-    </div>
-  );
-}
+    const [callState, setCallState] = useState("idle");
+  
+    const endCall = useCallback(() => {
+        setCallState("idle");
+        props.setPhoneNumber("");
+      }, [props]);
+  
+    useEffect(() => {
+      if (callState === "calling") {
+        const timerId = setTimeout(() => {
+          endCall();
+        }, 5000);
+  
+        return () => {
+          clearTimeout(timerId);
+        };
+      }
+    }, [callState, endCall]);
+  
+    const startCall = () => {
+      if (props.phoneNumber.length === 9) {
+        setCallState("calling");
+        props.setPhoneNumber("");
+      }
+    };
+  
+    return (
+      <div className="actions">
+        {callState === "idle" && (
+          <button className="call-button" onClick={startCall}>
+            Call
+          </button>
+        )}
+        {callState === "calling" && (
+          <button className="hang-button" onClick={endCall}>
+            Hang
+          </button>
+        )}
+      </div>
+    );
+  }
+  
